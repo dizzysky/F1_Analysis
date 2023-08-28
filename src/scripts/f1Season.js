93f99cde1c115ee9b775aff1097df0aa0d47cc29
Sanjid <dsanjid@gmail.com>
2023-08-28 10:02:44 -0400

    moved datasets to different file

--- src/scripts/f1Season.js
+++ src/scripts/f1Season.js
@@ -1,62 +1,4 @@
-// import Chart from 'chart.js'; // Import Chart.js if not already imported
-const circuitImages = {
-    "jacarepagua": "https://upload.wikimedia.org/wikipedia/commons/7/75/Aut%C3%B3dromo_Internacional_Nelson_Piquet_in_Bras%C3%ADlia.svg",
-    "imola": "https://upload.wikimedia.org/wikipedia/commons/4/42/Imola.svg",
-    "monaco": "https://upload.wikimedia.org/wikipedia/commons/3/36/Monte_Carlo_Formula_1_track_map.svg",
-    "rodriguez": "https://upload.wikimedia.org/wikipedia/commons/e/ef/Aut%C3%B3dromo_Hermanos_Rodr%C3%ADguez.svg",
-    "villeneuve": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/%C3%8Ele_Notre-Dame_%28Circuit_Gilles_Villeneuve%29.svg/2560px-%C3%8Ele_Notre-Dame_%28Circuit_Gilles_Villeneuve%29.svg.png",
-    "detroit": "https://upload.wikimedia.org/wikipedia/commons/d/df/TrackMap_Detroit-2023.png",
-    "ricard": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Le_Castellet_circuit_map_Formula_One_2019_and_2021_with_corner_names_English_19_07_2021.svg/1920px-Le_Castellet_circuit_map_Formula_One_2019_and_2021_with_corner_names_English_19_07_2021.svg.png",
-    "silverstone": "https://upload.wikimedia.org/wikipedia/commons/b/bd/Silverstone_Circuit_2020.png",
-    "hockenheimring": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Hockenheim2012.svg/2560px-Hockenheim2012.svg.png",
-    "hungaroring": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Hungaroring.svg/1920px-Hungaroring.svg.png",
-    "spa": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Spa-Francorchamps_of_Belgium.svg/2560px-Spa-Francorchamps_of_Belgium.svg.png",
-    "monza": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Monza_track_map.svg/2880px-Monza_track_map.svg.png",
-    "estoril": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Estoril_track_map.svg/2880px-Estoril_track_map.svg.png",
-    "jerez": "https://upload.wikimedia.org/wikipedia/commons/2/26/Jerez_GP_Circuit_2004.png",
-    "suzuka": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Suzuka_circuit_map--2005.svg/2560px-Suzuka_circuit_map--2005.svg.png",
-    "adelaide": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Adelaide_%28long_route%29.svg/2560px-Adelaide_%28long_route%29.svg.png"
-
-}
-
-const driverHeadshots = {
-    "prost": "https://cdn.images.autosport.com/f1greatestdrivers/mug/1955022400.jpg",
-    "berger": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGKdVutc1doaeJGomCW6sXtWArB_b_s9ClSYME48lBNM5FJdWk5NKn0C9zxKhswt_jwpo&usqp=CAU",
-    "senna": "https://s.wsj.net/public/resources/images/B3-DW970_CAPTAI_FR_20190503142818.jpg"
-
-}
-
-
-const podiums1988 = [
-    { constructor: 'McLaren-Honda', podiums: 25 },
-    { constructor: 'Ferrari', podiums: 10 },
-    { constructor: 'Lotus-Honda', podiums: 3 },
-    { constructor: 'Benetton-Ford', podiums: 3 },
-    { constructor: 'Arrows-Megatron', podiums: 1 },
-    { constructor: 'March-Judd', podiums: 1 }
-];  
-
-
-const constructorColors = {
-    'McLaren-Honda': '#c20619',
-    'Ferrari': '#ff2800',
-    'Lotus-Honda': '#00A800',
-    'Arrows-Megatron': '#6a160b',
-    'March-Judd': '#9cd5e8'
-
-    // etc.
-};
-
-const sampleData = [
-    { x: 1, y: 10 },
-    { x: 2, y: 15 },
-    { x: 3, y: 5 },
-    { x: 4, y: 30 },
-    { x: 5, y: 12 }
-];
-
-
-
+import { circuitImages, driverHeadshots, podiums1988, constructorColors } from "./dataset";
 
 export default class F1Season {
     constructor(mainElement, races) {
@@ -66,8 +8,6 @@ export default class F1Season {
         this.initializeSeasonStats();
         this.initializeScatterPlot();  // Add this line here
     }
-
-
 
     initializeRaces() {
         const raceNav = document.getElementById('race-nav');
@@ -252,13 +192,4 @@ export default class F1Season {
                 // You can initialize the chart with empty or fallback data here if you want
             });
     }
-    
-    
-    
-    
-    
-
-
-
-    
 }
